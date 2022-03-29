34d38d07058a6b16839c50648de15c01bad9cb8a
jerrvonewing <jerrvonewing.cs@gmail.com>
2022-03-28 20:54:51 -0500

    Cart dropdown menu now shows image and price of item. Cart icon now increments when an item is added to the cart.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,11 @@
 import { useContext } from "react";
+
 import { CartContext } from "../../context/cart.context";
+
 import Button from "../button/button.component";
-import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -10,9 +13,13 @@ const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
